918b7c3ccd1c99d9d620d30a25ea834d3bb0608c
Temirbay Abuali <114634176+TemirbayAbuali@users.noreply.github.com>
2024-08-03 06:11:35 +0500

    Update rock-paper-scissors.js

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -11,41 +11,28 @@ function playGame(playerMove) {
 
   let result = '';
 
-  if (playerMove === 'scissors') {
-    if (computerMove === 'rock') {
-      result = 'You lose.';
-    } else if (computerMove === 'paper') {
-      result = 'You win.';
-    } else if (computerMove === 'scissors') {
-      result = 'Tie.';
-    }
-
-  } else if (playerMove === 'paper') {
-    if (computerMove === 'rock') {
-      result = 'You win.';
-    } else if (computerMove === 'paper') {
-      result = 'Tie.';
-    } else if (computerMove === 'scissors') {
-      result = 'You lose.';
-    }
-
-  } else if (playerMove === 'rock') {
-    if (computerMove === 'rock') {
-      result = 'Tie.';
-    } else if (computerMove === 'paper') {
-      result = 'You lose.';
-    } else if (computerMove === 'scissors') {
-      result = 'You win.';
-    }
-  }
-
-  if (result === 'You win.') {
-    score.wins += 1;
-  } else if (result === 'You lose.') {
-    score.losses += 1;
-  } else if (result === 'Tie.') {
-    score.ties += 1;
-  }
+  result = 
+          playerMove === "rock" ?
+           
+            computerMove === "rock"  ? "Tie."
+          : computerMove === "paper" ? "You lost."
+          :                            "You won."
+        
+        : playerMove === "paper" ?
+           
+            computerMove === "rock"  ? "You won."
+          : computerMove === "paper" ? "Tie."
+          :                            "You lost."
+        
+        :
+           
+            computerMove === "rock"  ? "You lost."
+          : computerMove === "paper" ? "You won."
+          :                            "Tie.";
+        
+          result === "You won."  ? score.wins++
+        : result === "You lost." ? score.losses++
+        :                          score.ties++
 
   localStorage.setItem('score', JSON.stringify(score));
 
